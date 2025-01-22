a3d6b6533ffececf1be502c8b37e15facd22d651
Harsha <dharsha227@gmail.com>
2025-01-22 15:43:57 +0530

    decoration

--- lib/SecondScreen.dart
+++ lib/SecondScreen.dart
@@ -37,61 +37,69 @@ class _SecondscreenState extends State<Secondscreen> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: name,
-                decoration: const InputDecoration(hintText: "User name"),
-              ),
-              TextFormField(
-                controller: email,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  controller: name,
+                  decoration: InputDecoration(
+                    labelText: 'name',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-                keyboardType: TextInputType.emailAddress,
-                validator: validateEmail,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text("Selected Courses"),
-              SizedBox(
-                height: 20,
-              ),
-              ListView.builder(
-                  itemCount: widget.selectedCourse.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(widget.selectedCourse[index].name),
-                      subtitle: Text(widget.selectedCourse[index].duration),
-                    );
-                  }),
-              ElevatedButton(
-                  onPressed: () async {
-                    if (name.text == "" || email.text == "") {
-                      Get.snackbar("please fill all fields", "fill");
-                      return;
-                    }
-                    if (validateEmail(email.text) != null) {
-                      Get.snackbar("email not valid", "fill");
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: email,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: validateEmail,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("Selected Courses"),
+                SizedBox(
+                  height: 20,
+                ),
+                ListView.builder(
+                    itemCount: widget.selectedCourse.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(widget.selectedCourse[index].name),
+                        subtitle: Text(widget.selectedCourse[index].duration),
+                      );
+                    }),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (name.text == "" || email.text == "") {
+                        Get.snackbar("please fill all fields", "fill");
+                        return;
+                      }
+                      if (validateEmail(email.text) != null) {
+                        Get.snackbar("email not valid", "fill");
 
-                      return;
-                    }
-                    var res = await submitCources(
-                        name.text,
-                        email.text,
-                        widget.selectedCourse
-                            .map((course) => course.id)
-                            .toList());
-                    if (res) {
-                      Get.snackbar("Success", "Api call successfull");
-                      setState(() {});
-                    }
-                  },
-                  child: const Text("send data"))
-            ],
+                        return;
+                      }
+                      var res = await submitCources(
+                          name.text,
+                          email.text,
+                          widget.selectedCourse
+                              .map((course) => course.id)
+                              .toList());
+                      if (res) {
+                        Get.snackbar("Success", "Api call successfull");
+                        setState(() {});
+                      }
+                    },
+                    child: const Text("send data"))
+              ],
+            ),
           ),
         ));
   }
