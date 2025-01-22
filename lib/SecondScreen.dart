23e57ab6524d3ff152003c880df10864e2a4b103
Harsha <dharsha227@gmail.com>
2025-01-22 15:40:27 +0530

    animated transition

--- lib/SecondScreen.dart
+++ lib/SecondScreen.dart
@@ -1,15 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/network.dart';
+import 'package:get/get.dart';
 
 class Secondscreen extends StatefulWidget {
-  const Secondscreen({super.key});
+  List<Course> selectedCourse;
+  Secondscreen({super.key, required this.selectedCourse});
 
   @override
   State<Secondscreen> createState() => _SecondscreenState();
 }
 
 class _SecondscreenState extends State<Secondscreen> {
+  TextEditingController name = new TextEditingController();
+  TextEditingController email = new TextEditingController();
+  String? validateEmail(String? value) {
+    // Check if email is empty
+    if (value == null || value.isEmpty) {
+      return 'Please enter an email address';
+    }
+
+    // Simple regex to validate the email format
+    final emailRegex =
+        RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+    if (!emailRegex.hasMatch(value)) {
+      return 'Please enter a valid email address';
+    }
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Submit form"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: name,
+                decoration: const InputDecoration(hintText: "User name"),
+              ),
+              TextFormField(
+                controller: email,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: validateEmail,
+              ),
+              ListView.builder(
+                  itemCount: widget.selectedCourse.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Text(widget.selectedCourse[index].name);
+                  }),
+              ElevatedButton(
+                  onPressed: () async {
+                    if (name.text == "" || email.text == "") {
+                      Get.snackbar("please fill all fields", "fill");
+                      return;
+                    }
+                    if (validateEmail(email.text) != null) {
+                      Get.snackbar("email not valid", "fill");
+
+                      return;
+                    }
+                    var res = await submitCources(
+                        name.text,
+                        email.text,
+                        widget.selectedCourse
+                            .map((course) => course.id)
+                            .toList());
+                    if (res) {
+                      Get.snackbar("Success", "Api call successfull");
+                      setState(() {});
+                    }
+                  },
+                  child: const Text("send data"))
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_app/SecondScreen.dart';
 import 'package:flutter_app/network.dart';
 import 'package:flutter_app/stateController.dart';
 import 'package:get/get.dart';
@@ -33,8 +34,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   bool loading = false;
   List<Course> courses = [];
-  TextEditingController name = new TextEditingController();
-  TextEditingController email = new TextEditingController();
+
   TextEditingController search = new TextEditingController();
 
   getCourses() async {
@@ -48,22 +48,6 @@ class _HomeState extends State<Home> {
     getCourses();
   }
 
-  String? validateEmail(String? value) {
-    // Check if email is empty
-    if (value == null || value.isEmpty) {
-      return 'Please enter an email address';
-    }
-
-    // Simple regex to validate the email format
-    final emailRegex =
-        RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
-    if (!emailRegex.hasMatch(value)) {
-      return 'Please enter a valid email address';
-    }
-
-    return null;
-  }
-
   List<Course> selectedCourse = [];
   int pageIndex = 0;
   @override
@@ -71,129 +55,83 @@ class _HomeState extends State<Home> {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          leading: pageIndex == 1
-              ? IconButton(
-                  onPressed: () {
-                    setState(() {
-                      pageIndex = 0;
-                    });
-                  },
-                  icon: const Icon(Icons.arrow_back))
-              : null,
-          title: const Text("Course Management"),
-        ),
-        body: pageIndex == 0
-            ? SingleChildScrollView(
-                child: Column(
+          appBar: AppBar(
+            leading: pageIndex == 1
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        pageIndex = 0;
+                      });
+                    },
+                    icon: const Icon(Icons.arrow_back))
+                : null,
+            title: const Text("Course Management"),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Expanded(
-                            child: TextField(
-                          controller: search,
-                          decoration: const InputDecoration(hintText: "Search"),
-                        )),
-                        IconButton(
-                            onPressed: () {
-                              String query = search.text;
-                            },
-                            icon: const Icon(Icons.search)),
-                      ],
-                    ),
-                    ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount:
-                            context.watch<stateController>().courses.length,
-                        itemBuilder: (context, index) {
-                          return CheckboxListTile(
-                            onChanged: (value) {
-                              if (value == true) {
-                                selectedCourse.add(Provider.of<stateController>(
-                                        context,
-                                        listen: false)
-                                    .courses[index]);
-                              } else {
-                                selectedCourse.remove(
-                                    Provider.of<stateController>(context,
-                                            listen: false)
-                                        .courses[index]);
-                              }
-                              setState(() {});
-                            },
-                            title: Text(Provider.of<stateController>(
-                              context,
-                            ).courses[index].name),
-                            subtitle: Text(Provider.of<stateController>(
-                              context,
-                            ).courses[index].duration),
-                            value: selectedCourse
-                                .contains(Provider.of<stateController>(
-                              context,
-                            ).courses[index]),
-                          );
-                        }),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          pageIndex = 1;
-                        });
-                      },
-                      child: const Text("Continue"),
-                    ),
+                    Expanded(
+                        child: TextField(
+                      controller: search,
+                      decoration: const InputDecoration(hintText: "Search"),
+                    )),
+                    IconButton(
+                        onPressed: () {
+                          String query = search.text;
+                        },
+                        icon: const Icon(Icons.search)),
                   ],
                 ),
-              )
-            : Column(
-                children: [
-                  TextField(
-                    controller: name,
-                    decoration: const InputDecoration(hintText: "User name"),
-                  ),
-                  TextFormField(
-                    controller: email,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.allow(
-                          RegExp(r'[a-zA-Z0-9@._-]')),
-                    ],
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: validateEmail,
-                  ),
-                  ListView.builder(
-                      itemCount: selectedCourse.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        return Text(selectedCourse[index].name);
-                      }),
-                  ElevatedButton(
-                      onPressed: () async {
-                        if (name.text == "" || email.text == "") {
-                          Get.snackbar("please fill all fields", "fill");
-                          return;
-                        }
-                        if (validateEmail(email.text) != null) {
-                          Get.snackbar("email not valid", "fill");
-
-                          return;
-                        }
-                        var res = await submitCources(name.text, email.text,
-                            selectedCourse.map((course) => course.id).toList());
-                        if (res) {
-                          Get.snackbar("Success", "Api call successfull");
-                          pageIndex = 0;
-                          selectedCourse.clear();
+                ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: context.watch<stateController>().courses.length,
+                    itemBuilder: (context, index) {
+                      return CheckboxListTile(
+                        onChanged: (value) {
+                          if (value == true) {
+                            selectedCourse.add(Provider.of<stateController>(
+                                    context,
+                                    listen: false)
+                                .courses[index]);
+                          } else {
+                            selectedCourse.remove(Provider.of<stateController>(
+                                    context,
+                                    listen: false)
+                                .courses[index]);
+                          }
                           setState(() {});
-                        }
-                      },
-                      child: const Text("send data"))
-                ],
-              ),
-      ),
+                        },
+                        title: Text(Provider.of<stateController>(
+                          context,
+                        ).courses[index].name),
+                        subtitle: Text(Provider.of<stateController>(
+                          context,
+                        ).courses[index].duration),
+                        value: selectedCourse
+                            .contains(Provider.of<stateController>(
+                          context,
+                        ).courses[index]),
+                      );
+                    }),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.to(
+                        transition: Transition.leftToRight,
+                        Secondscreen(
+                          selectedCourse: selectedCourse,
+                        ));
+                    // setState(() {
+                    //   pageIndex = 1;
+                    // });
+                  },
+                  child: const Text("Continue"),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
